4c3aa375f931bff1cdc006be8c19d5e1cdcb1be2
Mikhail Kasimov <mikhail.kasimov@gmail.com>
2022-05-01 23:00:16 +0300

    Add files via upload

---
+++ maltrail-ips.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ipset -q flush maltrail
+ipset -q create maltrail hash:net
+for ip in $(curl http://127.0.0.1:8338/fail2ban 2>/dev/null | grep -P '^[0-9.]+$'); do ipset add maltrail $ip; done
+iptables -I INPUT -m set --match-set maltrail src -j DROP
+exit 0
